c46e38ae6d5905f346fbe950ff798e7edf5fa47c
GuiCeccoli <Gceccoli@proton.me>
2023-02-28 11:00:00 +0100

    Ajout d'une condition pour controler la requète API

--- front/js/script.js
+++ front/js/script.js
@@ -6,22 +6,27 @@ fetch("http://localhost:3000/api/products")
         if(res.ok){
         // Si l'api répond elle retourne le fichier au format JSON
         return res.json()
+        // On crée une liste avec les données récupérées
+    .then (data => {
+        data.map((products)=>{
+             document
+                 //On récupère l'élément HTML avec l'ID "items"
+                 .getElementById("items")
+                 // On intégre les données dans le fichier HTML
+                 .insertAdjacentHTML("beforeend", //Aligne les éléments convenablement
+                 `<a href="./product.html?id=${products.id}">
+                     <article>
+                       <img src="${products.imageUrl}" alt="${products.altTxt}">
+                       <h3 class="productName">${products.name} </h3>
+                       <p class="productDescription">${products.description}</p>
+                     </article>
+                   </a>`)
+             })  
+         })
+        } else {
+            alert("La requète a échouée")
         }
     })
-    // On crée une liste avec les données récupérées
-    .then (data => {
-       data.map((products)=>{
-            document
-                //On récupère l'élément HTML avec l'ID "items"
-                .getElementById("items")
-                // On intégre les données dans le fichier HTML
-                .insertAdjacentHTML("beforeend", //Aligne les éléments convenablement
-                `<a href="./product.html?id=${products.id}">
-                    <article>
-                      <img src="${products.imageUrl}" alt="${products.altTxt}">
-                      <h3 class="productName">${products.name} </h3>
-                      <p class="productDescription">${products.description}</p>
-                    </article>
-                  </a>`)
-            })  
-        })
+    .catch((error) => {
+        console.log(error)
+    })
